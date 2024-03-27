e9a35d7b4f04894d1b08350bb67411fbe099c9cb
rashedpro <rashedpro2025@gmail.com>
2024-03-28 00:15:30 +0300

    add log doctype

---
+++ pdf_on_submit/pdf_on_submit/doctype/whatsapp_log/test_whatsapp_log.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, Raffael Meyer and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestWhatsappLog(unittest.TestCase):
+	pass
--- pdf_on_submit/send_whatsapp.py
+++ pdf_on_submit/send_whatsapp.py
@@ -4,12 +4,18 @@ import frappe
 def send_whatsapp(doc,method=None):
     customer=frappe.get_doc("Customer",doc.customer)
     whatsapp_settings=frappe.get_doc("Whatsapp Setting")
-    document_url="https://business.tpf.deom.com.sa/files/{}.pdf".format(doc.name)
-    print("$$$$$$$$$$$document_url$$$$$$$$$$$")
-    print(document_url)
-    url = "https://api.4whats.net/sendFile/"
-    querystring = {"instanceid":whatsapp_settings.instance_id,"token":whatsapp_settings.token,"phone":customer.mobile_no,"body":document_url,"filename":doc.name,"caption":whatsapp_settings.caption}
-    response = requests.request("GET", url, params=querystring)
-    print("********************")
-    print(response.text)
+    if not whatsapp_settings.disable:
+        document_url="https://business.tpf.deom.com.sa/files/{}.pdf".format(doc.name)
+        url = "https://api.4whats.net/sendFile/"
+        querystring = {"instanceid":whatsapp_settings.instance_id,"token":whatsapp_settings.token,"phone":customer.mobile_no,"body":document_url,"filename":doc.name,"caption":whatsapp_settings.caption}
+        response = requests.request("GET", url, params=querystring)
+        json_response=frappe.parse_json(response.content.decode())
+        print("********************")
+        print(response.text)
+        doc_log = frappe.get_doc({
+                'doctype': 'Whatsapp Log',
+                "log":response.text,
+                "doctype_name":doc.name,
+                "status":json_response["sent"]})
+        doc_log.insert()
 
